bf710a847cdac0246b93c1aa7acee22b57390197
FedericoR95 <feder1995@outlook.it>
2020-12-19 16:56:13 +0100

    images revision

--- app.js
+++ app.js
@@ -4,51 +4,51 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [
     {
       name: 'Hogwarts',
-      img: 'images/Hogwarts.jpg'
+      img: 'images/Hogwarts.png'
     },
     {
       name: 'Hogwarts',
-      img: 'images/Hogwarts.jpg'
+      img: 'images/Hogwarts.png'
     },
     {
       name: 'Grifondoro',
-      img: 'images/Grifondoro.jpg'
+      img: 'images/Grifondoro.png'
     },
     {
       name: 'Grifondoro',
-      img: 'images/Grifondoro.jpg'
+      img: 'images/Grifondoro.png'
     },
     {
       name: 'Serpeverde',
-      img: 'images/Serpeverde.jpg'
+      img: 'images/Serpeverde.png'
     },
     {
       name: 'Serpeverde',
-      img: 'images/Serpeverde.jpg'
+      img: 'images/Serpeverde.png'
     },
     {
       name: 'Corvonero',
-      img: 'images/Corvonero.jpg'
+      img: 'images/Corvonero.png'
     },
     {
       name: 'Corvonero',
-      img: 'images/Corvonero.jpg'
+      img: 'images/Corvonero.png'
     },
     {
       name: 'Tassorosso',
-      img: 'images/Tassorosso.jpg'
+      img: 'images/Tassorosso.png'
     },
     {
       name: 'Tassorosso',
-      img: 'images/Tassorosso.jpg'
+      img: 'images/Tassorosso.png'
     },
     {
       name: 'Prefect',
-      img: 'images/Prefect.jpg'
+      img: 'images/Prefect.png'
     },
     {
       name: 'Prefect',
-      img: 'images/Prefect.jpg'
+      img: 'images/Prefect.png'
     }
   ]
 
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       var card = document.createElement('img')
-      card.setAttribute('src', 'images/HPback.jpg')
+      card.setAttribute('src', 'images/HPback.png')
       card.setAttribute('data-id', i)
       //card.addEventListener('click', flipcard)
       grid.appendChild(card)
